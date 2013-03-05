fa7b9e8ca3f11e7b16b99f3e4c9a891081ecf8f9
Victor Osimitz <v.osimitz@gmail.com>
2013-03-04 22:07:48 -0800

    very ugly first cut of "display all meals near me" and "add a meal"
    This is really just an end-to-end test of create-meal and
    display-meals, it's really ugly and hacky but it demonstrates data
    flowing from the client all the way to the DB, so huzzah.

--- breadwithfriends/breadwithfriends.js
+++ breadwithfriends/breadwithfriends.js
@@ -26,10 +26,21 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.mealsNearMe.meals = function(){
+    return Meals.find(); //TODO make this smarter
+  };
+
   Template.mealsNearMe.events({
-    'click #logout' : function () {
-      // template data, if any, is available in 'this'
-      switchToSplashScreen();
+    'click #create-meal' : function () {
+      options={title:"Thai Food with Skiers",
+               description:"Eat Thai food and talk about skiing",
+               time:"03/18/2013 7:00 PM",
+               location:{address:"2850 Middlefield Rd #227", city:"Palo Alto", state:"CA", zip:"94306"},
+               guests:{},
+               public:true
+              };
+      Meteor.call("createMeal", options);
+      Meteor.call("createUserDetails", {location:{city:"Stanford",state:"CA",zip:"94305"}});
     }
   });
 
