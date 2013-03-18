53e013d875b049374bafb930435f15b02613eef6
Victor Osimitz <v.osimitz@gmail.com>
2013-03-18 16:23:03 -0700

    manage invitations: invite more people, view invites and status

--- breadwithfriends/model/invitations.js
+++ breadwithfriends/model/invitations.js
@@ -2,6 +2,7 @@
    event: id of the event,
    inviting_user: usually the host of the event,
    invited_user: the id OR email address of the user invited to the event,
+          in a format like {email: "a@b.c"} or {user_id:"xyz"}
    response: may be null, otherwise one of [yes, no]
 */
 
@@ -30,24 +31,35 @@ Invitations.validateInvitation = function(invitation)
   //that it is one or the other
   if(!invitation.invited_user)
     return false; //invited user not specified
+  if(!invitation.invited_user.email && !invitation.invited_user.uid)
+    return false; //invited user is specified but not valid
   if(invitation.response && invitation.response != "yes" && invitation.response != "no")
     return false; //invitation response is set, but invalid
   return true; //must be good to go if we get this far
 };
 
 Meteor.methods({
-  createInvitations: function(invite_set){
+  createInvitations: function(invite_set){ //right now we assume users are invited by email address
     invite_set.inviting_user = invite_set.inviting_user || this.userId;
-    console.log(JSON.stringify(invite_set));
+    //console.log(JSON.stringify(invite_set));
     for(i in invite_set.invited_users)
     {
       invited_user = invite_set.invited_users[i].trim();
       invitation = {event:invite_set.event,
                     inviting_user:invite_set.inviting_user,
-                    invited_user:invited_user};
+                    invited_user:{email:invited_user}};
       if(!Invitations.validateInvitation(invitation)) //log an error and keep going
         console.log("Invalid invitation: " + JSON.stringify(invitation));
       Invitations.insert(invitation);
     }
+  },
+  createInvitation: function(invitation){
+    invitation.inviting_user = invitation.inviting_user || this.userId;
+    if(!Invitations.validateInvitation(invitation))
+    {
+      console.log("Invalid invitation: " + JSON.stringify(invitation));
+      throw new Meteor.Error(400, "Invalid invitation");
+    }
+    Invitations.insert(invitation);
   }
 });
--- breadwithfriends/screens/eventInvites.js
+++ breadwithfriends/screens/eventInvites.js
@@ -12,7 +12,22 @@ if(Meteor.isClient)
 	return Meals.findOne(evt_id);
   };
   
+  Template.eventInvitesScreen.invites = function()
+  {
+    evt_id = Session.get("showScreen").event_id;
+    return Invitations.find({event:evt_id});
+  };
+  
   Template.eventInvitesScreen.events({
+    'click #add_invite': function()
+    {
+      email = document.getElementById("new_invite").value.trim();
+      evt_id = Session.get("showScreen").event_id;
+      invite = {event:evt_id,invited_user:{email:email}};
+      Meteor.call("createInvitation",invite,function(error, result){
+        if(!error) document.getElementById("new_invite").value="";
+      });
+    },
     'click #show_event': function()
     {
       switchToMealDetailsScreen(Session.get("showScreen").event_id);
@@ -22,4 +37,15 @@ if(Meteor.isClient)
       switchToMealsNearMeScreen();
     }
   });
+  
+  Template.invite.invited_name = function()
+  {
+    return this.invited_user.email;
+  };
+  
+  Template.invite.rsvp = function()
+  {
+    if(this.response) return response;
+    else return "";
+  };
 }
